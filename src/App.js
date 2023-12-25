64bcea327ce6981b6a2d6e0f0f84854c0f84c1cb
Aleksandr Sapelnikov <sapelnikv-aleksandr@yandex.ru>
2023-12-25 14:32:14 +0300

    add state, render

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const App = (props) => {
   return (
-      <BrowserRouter>
         <div className='app-wrapper'>
           <Header/>
           <Navbar/>
           <div className='app-wrapper-content'>
               <Routes>
-                <Route path='/dialogs' element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
-                <Route path='/profile' element={<Profile posts={props.posts}/>}/>
+                <Route path='/dialogs' element={<Dialogs state={props.state.dialogsPage}/>}/>
+                <Route path='/profile' element={<Profile state={props.state.profilePage}
+                                                         addPost={props.addPost}/>}/>
               </Routes>
           </div>
         </div>
-      </BrowserRouter>)
+      )
 }
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = (props) => {
         <div>
             <h1>Профиль</h1>
             <ProfileInfo/>
-            <MyPosts posts={props.posts}/>
+            <MyPosts posts={props.state.posts} addPost={props.addPost}/>
         </div>
     )
 }
